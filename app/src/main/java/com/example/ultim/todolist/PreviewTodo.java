e94a99382411a7018ece0d54c5197e0418529f4d
ultimatum424 <Good20032014>
2017-02-19 18:22:45 +0300

    Add Preview layout

---
+++ app/src/main/java/com/example/ultim/todolist/PreviewTodo.java
@@ -0,0 +1,13 @@
+package com.example.ultim.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreviewTodo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_todo);
+    }
+}
